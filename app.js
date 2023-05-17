4962b39792c8a72a26d84ef8c413902c35eb49d5
goodman2814 <mgoodaman@gmail.com>
2023-05-17 14:51:39 -0400

    Added JS to show value of dice after a roll

--- app.js
+++ app.js
@@ -8,36 +8,47 @@ var diceFaces = [
   "https://bit.ly/dice-six",
 ];
 
-var imageDiceOne = document.getElementById("dice01");
-var imageDiceTwo = document.getElementById("dice02");
-var audio = document.getElementById("audio");
+let imageDiceOne = document.getElementById("dice01");
+let imageDiceTwo = document.getElementById("dice02");
+let resultDice = document.querySelector(".diceValue");
+let audio = document.getElementById("audio");
 
 // Gets a random integer, min & max inclusive
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  result = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(result);
+  return result;
 }
+
 // Roll dice function
 function rollThatDice() {
   audio.currentTime = 0;
   imageDiceOne.style.animation = "spindice 0.25s";
+
   setTimeout(function () {
     imageDiceOne.setAttribute("src", diceFaces[getRandomInt(1, 6)]);
+    let c = result;
+  }, 150);
+
+  setTimeout(function () {
+    imageDiceTwo.setAttribute("src", diceFaces[getRandomInt(1, 6)]);
+    let d = result;
+    resultDice.innerHTML = c + " + " + d + " = " + c * d;
   }, 150);
 
   setTimeout(function () {
     imageDiceOne.style.animation = "none";
   }, 250);
-  audio.play();
-  imageDiceTwo.style.animation = "spindice 0.25s";
-  setTimeout(function () {
-    imageDiceTwo.setAttribute("src", diceFaces[getRandomInt(1, 6)]);
-  }, 150);
 
+  audio.play();
+
+  imageDiceTwo.style.animation = "spindice 0.25s";
   setTimeout(function () {
     imageDiceTwo.style.animation = "none";
   }, 250);
+
   audio.play();
 }
 
