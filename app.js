563e5be95959a55742a97698dbe9c44dbc99d1fe
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-05-17 10:15:49 -0300

    add countdown timer to the game page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var diceFaces = [
+let diceFaces = [
   "https://bit.ly/dice-unknown",
   "https://bit.ly/dice-one",
   "https://bit.ly/dice-two",
@@ -8,10 +8,34 @@ var diceFaces = [
   "https://bit.ly/dice-six",
 ];
 
-var imageDiceOne = document.getElementById("dice01");
-var imageDiceTwo = document.getElementById("dice02");
-var audio = document.getElementById("audio");
+let imageDiceOne = document.getElementById("dice01");
+let imageDiceTwo = document.getElementById("dice02");
+let audio = document.getElementById("audio");
 
+// function sets minutes and seconds
+function startTimer(duration, display) {
+  let timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+    if (--timer < 10) {
+      timer = duration;
+    }
+  }, 2000);
+}
+// add the countdown time to the HTML page and start regressive time
+function startCount() {
+  let duration = 60 * 2; // transform seconds to minutes
+  let display = document.querySelector("#countdown");
+  startTimer(duration, display);
+}
 // Gets a random integer, min & max inclusive
 function getRandomInt(min, max) {
   min = Math.ceil(min);
