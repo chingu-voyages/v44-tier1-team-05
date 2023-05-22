928142e5448aa4b793b2872780f94f05ff846cc3
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-05-22 14:55:30 -0300

    fix a bug on clear button

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ function getRandomInt(min, max) {
 // Roll dice function
 function rollThatDice() {
   audio.currentTime = 0;
+  document.getElementById("error").textContent = " ";
   imageDiceOne.style.animation = "spindice 0.25s";
   let diceOneValue = getRandomInt(1, 6);
   setTimeout(function () {
