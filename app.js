52fc93512bd7afbce4a93c08cbda0025f2da23f1
Nazgul Duisenbay <nazgul7d@gmail.com>
2023-05-25 16:00:46 -0400

    Bug on timer

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function startTimer(duration, display) {
   let timer = duration,
     minutes,
     seconds;
-  setInterval(function () {
+  let intervalId = setInterval(function () {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
 
@@ -68,8 +68,10 @@ function startTimer(duration, display) {
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     display.textContent = minutes + ":" + seconds;
-    if (--timer < 10) {
-      timer = duration;
+    if (timer <= 0) {
+      clearInterval(intervalId); // Stop the timer when it reaches 0
+    } else {
+      timer--;
     }
   }, 1000);
 }
